af68bbf37ef38f939e57b99cc11c45e5c9dbb83a
배승철 <paedison@gmail.com>
2023-02-26 17:32:00 +0900

    settings 변경

---
+++ mysite.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd "C:/Users/New Shin PSAT1/PycharmProjects/pybo/mysite"
+set DJANGO_SETTINGS_MODULE=config.settings.local
+start /D "C:/Users/New Shin PSAT1/PycharmProjects/pybo/venv/Scripts/" activate
